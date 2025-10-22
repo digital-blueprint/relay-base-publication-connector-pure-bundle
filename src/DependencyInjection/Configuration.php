115eb6ca83dadf0a0926d00f0b08427146acc496
Arslan Khurshid <arslan.khurshid@tugraz.at>
2025-10-22 12:53:02 +0200

    config update

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,24 +4,11 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\BasePublicationConnectorPureBundle\DependencyInjection;
 
-use Dbp\Relay\CoreBundle\Authorization\AuthorizationConfigDefinition;
-use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    public const ROLE_USER = 'ROLE_USER';
-    public const ROLE_ADMIN = 'ROLE_ADMIN';
-
-    private function getAuthorizationConfigNode(): NodeDefinition
-    {
-        return AuthorizationConfigDefinition::create()
-            ->addRole(self::ROLE_USER, 'false', 'Returns true if the user is allowed to use the API.')
-            ->addRole(self::ROLE_ADMIN, 'false', 'Returns true if the user has unrestricted access to the API.')
-            ->getNodeDefinition();
-    }
-
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('dbp_relay_base_publication_connector_pure');
@@ -43,7 +30,6 @@ class Configuration implements ConfigurationInterface
             ->end()
             ->end()
             ->end()
-            ->append($this->getAuthorizationConfigNode())
             ->end();
 
         return $treeBuilder;
